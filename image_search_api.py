df381f6719601d2f40c4caab6e52eecddcb9ea9c
zeeshan2221 <121931377+zeeshan2221@users.noreply.github.com>
2023-03-29 02:25:06 +0500

    Add files via upload

--- image_search_api.py
+++ image_search_api.py
@@ -1,27 +1,17 @@
-import requests
+import openai
+from env import OPENAI_API_KEY
 
-from env import GCD_API_KEY
+# Set up the OpenAI API client
+openai.api_key = OPENAI_API_KEY
 
 def get_images(query):
-  # Get the images from the google custom search api
-  # Google only gives 100 call/day, so be mindful when testing hehe :)
-  get_url = "https://www.googleapis.com/customsearch/v1"
-  params = {
-    "q": query,
-    "num": 1,
-    "start": 1,
-    "imgSize": "large",
-    "searchType": "image",
-    "filetype": "jpg",
-    "key": GCD_API_KEY,
-    "cx": "47b694f30fc314a0b"
-  }
-  response = requests.get(get_url, params=params)
-  response.raise_for_status()
-  search_results = response.json()
-  # Create a list with the links of the images gotten from the search_ and return it
-  image_links = []
-  for result in search_results['items']:
-    image_links.append(result['link'])
-  #Return a list of the links of the images
-  return image_links
+    # Use the OpenAI API to generate a list of image URLs
+    response = openai.Image.create(
+        prompt=f"Search for images of {query}",
+        n=1,
+        size="1024x1024"
+    )
+
+    # Extract the URLs from the response and return them as a list
+    image_links = [image.url for image in response.data]
+    return image_links
--- tasks.py
+++ tasks.py
@@ -10,7 +10,6 @@ import redis
 import rq
 
 from image_search_api import get_images
-from gcp_text_to_speech import synthesize_text_with_audio_profile
 from worker import conn
 
 r = redis.Redis(host='localhost', port=6379, db=0)
@@ -54,34 +53,16 @@ def get_retry():
 
 def generate_clip(text, ip, i, qid, language_id) -> dict: # instance path, index in slides, audio id, query id
   fn = get_audio_clip_path(ip, qid, i)
-  language_code = {
-    "en": "en-US",
-    "de": "de-DE",
-    "fr": "fr-FR",
-    "es": "es-ES",
-    "nl": "nl-NL",
-    "it": "it-IT",
-    "pt": "pt-PT",
-    "ru": "ru-RU",
-    "ja": "ja-JP",
-    "ko": "ko-KR",
-    "zh": "zh-CN",
-    "ar": "ar-AE",
-    "hi": "hi-IN",
-    "bn": "bn-IN",
-    "pa": "pa-IN",
-    "te": "te-IN",
-    "tr": "tr-TR",
-    "id": "id-ID",
-    "vi": "vi-VN",
-    "th": "th-TH",
-    "ms": "ms-MY",
-    "ur": "ur-PK",
-    "fa": "fa-IR",
-    "mr": "mr-IN",
-    "da": "da-DK",
-  }.get(language_id, "en-US")
-  synthesize_text_with_audio_profile(text, output=fn, language_code=language_code)
+  response = openai.Completion.create(
+      engine="text-davinci-002",
+      prompt=f"Please create an audio clip for the following text: {text}",
+      max_tokens=1024,
+      n=1,
+      stop=None,
+      temperature=0.7,
+  )
+  audio_text = response.choices[0].text
+  synthesize_text_with_audio_profile(audio_text, output=fn)
   return dict()
 
 def generate_image(query) -> dict:
@@ -91,7 +72,7 @@ def generate_image(query) -> dict:
 
 MAX_TRIES = 5
 def execute(task):
-  print("exeucting task", task)
+  print("executing task", task)
   try_i = 0
   while try_i < MAX_TRIES:
     try:
@@ -109,94 +90,4 @@ def generate_presentation(qid, query_text, user_prof, instance_path, language_id
   language_name = lang_id2natural_lang_lang(language_id)  
   query = f'Write content for a slide deck about {query_text} for a listener who is a {user_prof}. Write the result in {language_name}. Return a JSON list with data about each slide. The list consists of ' \
               f'objects with a "text" key containing a paragraph ' \
-              'that is spoken by the presenter, a "title" key to name the slide with low verbose, a "key_points" key which will contain a list of 3 key points (a couple of ' \
-              'words) to be shown on the slide, and an "image_query" key that will contain the name of an image to illustrate each point with low verbose'
-  response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
-    {"role": "user", "content": query},
-  ])
-
-  qck = qid2qck(qid)
-  presentation = r.get(qck)
-  presentation = json.loads(presentation) # TODO: if this fails, we should retry the api call.
-  presentation["original_query"] = query
-
-  print("got response", response)
-
-  response = response['choices'][0]['message']['content']
-  presentation["original_response"] = response
-  slides = json.loads(response)
-  for i in range(len(slides)):
-    slides[i]["audio_status"] = "pending"
-    slides[i]["image_status"] = "pending"
-    slides[i]["is_follow_up"] = False
-
-  tasks = []
-  for i, text in enumerate([slide["text"] for slide in slides]):
-    tasks.append(dict(func=generate_clip, kwargs=dict(text=text, i=i, qid=qid, language_id=language_id, ip=instance_path)))
-  for image_query in [slide["image_query"] for slide in slides]:
-    tasks.append(dict(func=generate_image, kwargs=dict(query=image_query)))
-
-  with multiprocessing.Pool(len(tasks)) as p:
-    results = p.map(execute, tasks)
-
-  print("results", results)
-
-  assert len(results) == len(slides) * 2
-  audio_rets = results[:len(slides)]
-  image_rets = results[len(slides):len(slides)*2]
-
-  for i, audio_ret in enumerate(audio_rets):
-    slides[i]["audio_status"] = audio_ret.get("status")
-  for i, image_ret in enumerate(image_rets):
-    slides[i]["image_url"] = image_ret.get("url")
-    slides[i]["image_status"] = image_ret.get("status")
-
-  print("made slides", slides)
-
-  presentation["slides"] = slides
-  presentation["status"] = "completed"
-  r.set(qid2qck(qid), json.dumps(presentation))
-
-def answer_follow_up_question(qid, slide_idx, question, instance_path, language_id):
-  language_name = lang_id2natural_lang_lang(language_id)  
-
-  qck = qid2qck(qid)
-  presentation = r.get(qck)
-  presentation = json.loads(presentation)
-  slide_title = presentation["slides"][slide_idx]["title"]
-
-  response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
-      {"role": "user", "content": presentation["original_query"]},
-      {"role": "assistant", "content": presentation['original_response']},
-      {"role": "user", "content": f'The listener has a question about the slide about {slide_title}. The listener says "{question}" Create a new slide, using the same structure as the other slides, to answer this question. Again answer in {language_name}. Return just the JSON dictionary for the new slide..'}])
-  print("got response to follow up question:", response)
-  response = response['choices'][0]['message']['content']
-  print("got response to follow up question: ", response)
-  new_slide = json.loads(response)
-  try:
-    new_slide = new_slide[0]
-  except:
-    pass
-
-  new_slide["audio_status"] = "pending"
-  new_slide["image_status"] = "pending"
-  new_slide["is_follow_up"] = True
-
-  new_slide_idx = slide_idx + 1
-
-  presentation["slides"].insert(new_slide_idx, new_slide)
-  # rename the audio files
-  for i in reversed(range(new_slide_idx, len(presentation["slides"]) - 1)): # len -1 because we just added a new slide
-    old_fn = get_audio_clip_path(instance_path, qid, i)
-    new_fn = get_audio_clip_path(instance_path, qid, i+1)
-    os.rename(old_fn, new_fn)
-    print("renamed", old_fn, "to", new_fn)
-  presentation["status"] = "completed"
-
-  r.set(qck, json.dumps(presentation)) 
-
-  q.enqueue_call(generate_clip, args=(new_slide["text"], instance_path, new_slide_idx, qid, language_id), retry=get_retry())
-  q.enqueue_call(generate_image, args=(new_slide["image_query"], new_slide_idx, qid), retry=get_retry())
-
-def get_audio_clip_path(ip, qid, i):
-  return os.path.join(ip, "audio", f"{qid}-{i}.mp3")
+              'that is spoken by the presenter, a "title" key to name the slide with low verbose, a "key_points" key which
