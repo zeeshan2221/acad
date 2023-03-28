4392007de557e1f0a95adb6403d70e6b5f61cf9c
zeeshan2221 <121931377+zeeshan2221@users.noreply.github.com>
2023-03-29 02:23:08 +0500

    Add files via upload

--- worker.py
+++ worker.py
@@ -1,3 +1,4 @@
+
 import os
 
 import redis
@@ -5,12 +6,15 @@ from rq import Worker, Queue, Connection
 
 listen = ['default']
 
+if not os.getenv('OPENAI_API_KEY'):
+    print('Error: OpenAI API key is not set.')
+    exit()
+
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
-  with Connection(conn):
-    worker = Worker(list(map(Queue, listen)))
-    worker.work()
-
+    with Connection(conn):
+        worker = Worker(list(map(Queue, listen)))
+        worker.work()
